3422cb25e0579d75dc4ba37a0c2e50b4abf7e062
akrck02 <aketza.vazquez@gmail.com>
2025-01-03 20:29:32 +0100

    lsp: ts

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,16 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "gopls"}
+local servers = {
+  "html",
+  "cssls",
+  "gopls",
+--  "kotlin-language-server",
+--  "java-language-server",
+--  "rust-analyzer",
+--  "gdtoolkit",
+--  "gradle-language-server"
+}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -17,8 +26,8 @@ for _, lsp in ipairs(servers) do
 end
 
 -- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- } 
+lspconfig.ts_ls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+} 
